0d410da63e00ce0f3cc0ec6734d26167674adf10
leo <heiberescalona06@gmail.com>
2024-10-26 10:51:08 -0400

    termine

--- scrip.js
+++ scrip.js
@@ -1,7 +1,11 @@
-for (let i = 1; i <= 20; i++) {
-    if (i % 2 === 0) {
-        console.log(i + " es par");
-    } else {
-        console.log(i + " es impar");
+function imprimirNumeros() {
+    let resultado = "";
+    for (let i = 1; i <= 20; i++) {
+        if (i % 2 === 0) {
+            resultado += i + " es par<br>";
+        } else {
+            resultado += i + " es impar<br>";
+        }
     }
+    document.getElementById("resultado").innerHTML = resultado;
 }
